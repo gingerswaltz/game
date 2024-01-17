e212d19e87e420010e7718483c2851b6a8dbdff5
gingerswaltz <115674180+gingerswaltz@users.noreply.github.com>
2024-01-17 20:28:37 +0900

    init commit
    проблемы с рутами, надо смотреть все делать порядочно

---
+++ server/models.js
@@ -0,0 +1,90 @@
+// models.js
+
+
+const { Sequelize, DataTypes } = require('sequelize')
+
+// Инициализация Sequelize с параметрами подключения к базе данных
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '1',
+    database: 'game_db',
+});
+
+// Таблица игроков
+const Player = sequelize.define('Player', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+// Таблица игр
+const Game = sequelize.define('Game', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    winnerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Player,
+            key: 'id',
+        },
+    },
+    loserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Player,
+            key: 'id',
+        },
+    },
+    draw: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+});
+
+
+// Таблица рейтинга
+const Rating = sequelize.define('Rating', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    playerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Player,
+            key: 'id',
+        },
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+});
+
+// Синхронизация с базой данных, создание таблиц, если их нет
+sequelize.sync();
+
+
+// Экспорт моделей
+module.exports={
+    Player,
+    Game,
+    Rating,
+    sequelize,
+};
